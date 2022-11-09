fc0908c4683e20e914cddc21c56721bb9b977f0b
Alex <aleksander.wos@onet.eu>
2022-11-09 10:57:57 +0100

    jedzi nielze

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ color_name_value_dict = {
 }
 
 
-BLACKS = [1, 7]
+BLACKS = ['Black','Brown']
 YELLOWS = [4]
 
 
@@ -62,8 +62,14 @@ i = 0
 
 # 2 białe - prosto
 # jak prawy będzie czarny i lewy biały -> skręć w prawo
-speed_straigt = 40
+
+# wolne w maire dziala
+speed_straigt = 35
 speed_turn = 20
+SLEEP_TIME = 0.15
+
+# speed_straigt = 25
+# speed_turn = 15
 
 state_dict = {
     0: 'prosto',
@@ -77,45 +83,69 @@ state_dict = {
 # green = green
 
 def print_colors(r_color, l_color):
-    print("prawy kolor: {}".format(color_value_name_dict[r_color]))
-    print("lewy kolor: {}".format(color_value_name_dict[l_color]))
+    # print("prawy kolor: {}".format(color_value_name_dict[r_color]))
+    # print("lewy kolor: {}".format(color_value_name_dict[l_color]))
+    print("prawy kolor: {}".format(r_color))
+    print("lewy kolor: {}".format(l_color))
 
 
 while True:
     try:
-        if right_sensor.color in BLACKS and color_value_name_dict[left_sensor.color] == 'White':
+        left_color = color_value_name_dict[left_sensor.color]
+        right_color = color_value_name_dict[right_sensor.color]
+        if right_color in BLACKS and left_color == 'White':
             while True:
-                # sleep(0.2)
-                print("skrecam w prawo")
-                print_colors(right_sensor.color, left_sensor.color)
 
-                right_motor.on(SpeedPercent(-speed_turn//2))
-                left_motor.on(SpeedPercent(speed_turn))
-                if color_value_name_dict[right_sensor.color] == 'White' and color_value_name_dict[left_sensor.color] == 'White':
-                    break
-                if right_sensor.color in BLACKS and left_sensor.color in BLACKS:
-                    break
-        elif color_value_name_dict[right_sensor.color] == 'White' and left_sensor.color in BLACKS:
-            while True:
-                # sleep(0.2)
                 
-                print("skrecam w lewo")
-                print_colors(right_sensor.color, left_sensor.color)
+                print_colors(right_color, left_color)
+                print("skrecam w prawo")
 
-                left_motor.on(SpeedPercent(-speed_turn//2))
-                right_motor.on(SpeedPercent(speed_turn))
-                if color_value_name_dict[right_sensor.color] == 'White' and color_value_name_dict[left_sensor.color] == 'White':
+                right_motor.on(SpeedPercent(-speed_turn))
+                left_motor.on(SpeedPercent(speed_turn))
+
+                # sleep(SLEEP_TIME)
+                left_color = color_value_name_dict[left_sensor.color]
+                right_color = color_value_name_dict[right_sensor.color]
+                
+
+                print_colors(right_color, left_color)
+
+                if right_color == 'White' and left_color == 'White':
                     break
-                if right_sensor.color in BLACKS and left_sensor.color in BLACKS:
+
+                # zapobiega wyjściu na skrzyżowaniu
+                if right_color in BLACKS and left_color in BLACKS:
+                    break
+        # skręt w lewo
+        elif right_color == 'White' and left_color in BLACKS:
+            while True:
+                
+                
+                print_colors(right_color, left_color)
+                print("skrecam w lewo")
+
+                left_motor.on(SpeedPercent(-speed_turn))
+                right_motor.on(SpeedPercent(speed_turn))
+
+                # sleep(SLEEP_TIME)
+                left_color = color_value_name_dict[left_sensor.color]
+                right_color = color_value_name_dict[right_sensor.color]
+
+                print_colors(right_color, left_color)
+
+                if right_color == 'White' and left_color == 'White':
+                    break
+                if right_color in BLACKS and left_color in BLACKS:
                     break
 
         else:
+            
+            print_colors(right_color, left_color)
             print("jade prosto")
-            print_colors(right_sensor.color, left_sensor.color)
 
             right_motor.on(SpeedPercent(speed_straigt))
             left_motor.on(SpeedPercent(speed_straigt))
-        sleep(0.1)
+        # sleep(0.1)
 
 
     except (OSError, ValueError) as e:
